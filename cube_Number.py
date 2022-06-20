a8bfeec5ad6ab3a6f00ed54fdb8a14becb9c6d2d
shaik1121 <sk.muzakkirhussain@gmail.com>
2022-06-21 03:11:55 +0530

    day_21-06

--- cube_Number.py
+++ cube_Number.py
@@ -1,9 +1,9 @@
-#Calculate the cube of all numbers from 1 to a given number
-#Write a program to rint the cube of all numbers from 1 to a given number
+# Calculate the cube of all numbers from 1 to a given number
+# Write a program to rint the cube of all numbers from 1 to a given number
 from re import I
 
 n = int(input("Value: "))
 for i in range(1, n+1):
-  #logic for finding cube of number
-  cub = i*i*i
-  print("Current Number is: ", i, "and the cube is", cub)
+    # logic for finding cube of number
+    cub = i * i * i
+    print("Current Number is: ", i, "and the cube is", cub)
--- odd index position.py
+++ odd index position.py
@@ -1,5 +1,6 @@
-#Use a loop to display elements from a given list present at odd index positions
+# Use a loop to display elements from a given list present at odd index positions
 my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-#we are starting an index with one then we are using "::" for steping and we are steping 2.
+# we are starting an index with one then we are using "::" for steping and we are steping 2.
 for i in my_list[1::2]:
   print(i, end=" ")
+        
